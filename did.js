c31468ebc2596d6439a80c98316b2e5b23a3d950
asdfg603174 <asdfg603174@gmail.com>
2024-10-20 22:39:45 +0800

    first commit

---
+++ did.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        name:'123',
+        price: 100,
+    },
+
+    {
+        name: '456',
+        price: 150,
+    },
+
+    {
+        name: '789',
+        price: 200,
+    },
+
+
+];
+let shoppingCart = [];
+let total = 0;
