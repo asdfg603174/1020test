f3f8f22b5fef2f75ae4b9708ae56df00d73983cb
asdfg603174 <asdfg603174@gmail.com>
2024-10-20 23:29:35 +0800

    1129

--- did.js
+++ did.js
@@ -1,20 +1,35 @@
 const products = [
     {
-        name:'123',
+        name:'鍋燒意麵',
         price: 100,
     },
 
     {
-        name: '456',
-        price: 150,
+        name: '起司奶油鍋燒意麵',
+        price: 120,
     },
 
     {
-        name: '789',
-        price: 200,
+        name: '韓式泡菜鍋燒意麵',
+        price: 115,
     },
 
 
 ];
 let shoppingCart = [];
 let total = 0;
+
+
+const product1Btn = document.querySelector('#product1')
+const product2Btn = document.querySelector('#product2')
+const product3Btn = document.querySelector('#product3')
+
+product1Btn.addEventListener('click',function() {
+    shoppingCart.push(products[0])
+    
+    total = shoppingCart.reduce((pre,curr) => {
+        return pre + curr.price;
+
+    }, 0);
+    console.log(shoppingCart,total)
+})
